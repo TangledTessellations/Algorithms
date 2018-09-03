b13b1481b9aaa0d589ccfb14f6e412d1f2a8cc7e
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-03 13:42:51 -0700

    Original iterative solution to making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,26 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass
+class MakingChange:
+  def __init__(self):
+    self.cache = []
+  
+  def making_change(self, amount, denominations):
+    self.cache = [0 for i in range(amount + 1)]
+    self.cache[0] = 1
+    for coin in denominations:
+      for higher_amount in range(coin, amount + 1):
+        self.cache[higher_amount] = self.cache[higher_amount] + self.cache[higher_amount - coin]
+    return self.cache[amount]
 
 
 if __name__ == "__main__":
   # Test out your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
+  change_maker = MakingChange()
   if len(sys.argv) > 1:
     denominations = [1, 5, 10, 25, 50]
     amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+    print("There are {ways} ways to make {amount} cents.".format(ways=change_maker.making_change(amount, denominations), amount=amount))
   else:
     print("Usage: making_change.py [amount]")
