ed649668d98625cc407c30fa45af050e00b2a937
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-03 10:50:39 -0700

    Refactored climbing_stairs.py

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -4,28 +4,17 @@ import sys
 
 class ClimbingStairs:
   def __init__(self):
-    self.cache = None
+    self.cache = {0: 0, 1: 1, 2: 1}
 
-  def climbing_stairs(self, n, rangeIn=None):
-    if rangeIn:
-      self.cache = {i: 0 for i, val in enumerate(rangeIn)}
-    else:
-      self.cache = {i: 0 for i in range(n + 1)}
-
-    return self.climbing_stairs_cache(n)
-
-  def climbing_stairs_cache(self, n):
+  def climbing_stairs(self, n, uselessArray=None):
     if n < 2:
-      self.cache[n] = 1
       return self.cache[n]
     elif n == 2:
-      self.cache[2] = 2
       return self.cache[2]
-    else:
-      if self.cache[n] != 0:
+    elif n in self.cache:
         return self.cache[n]
-      else:
-        self.cache[n] = self.climbing_stairs_cache(n-1) + self.climbing_stairs_cache(n-2) + self.climbing_stairs_cache(n-3)
+    else:
+        self.cache[n] = self.climbing_stairs(n-1) + self.climbing_stairs(n-2) + self.climbing_stairs(n-3)
     return self.cache[n]
 
 if __name__ == "__main__":
